6d40bb4f5aefe514491436fa8b3b06fe7723c142
Dustin <dustin.dumenigo@abstracta.com.uy>
2022-04-25 17:10:22 -0300

    changes_in_mapsitepage

--- src/test/java/pageObjects/MapSitePage.java
+++ src/test/java/pageObjects/MapSitePage.java
@@ -1,4 +1,17 @@
 package pageObjects;
 
-public class MapSitePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class MapSitePage extends BasePage{
+    public MapSitePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    
+
+
+
+
+
 }
